6f59ae4c56368d4a95698192198373ace9ca3721
emreturgutce <emrebursalee@gmail.com>
2020-07-22 13:16:41 +0300

    location model

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/screens/location_detail/image_banner.dart';
 import 'package:hello_world/screens/location_detail/text_section.dart';
+import 'package:hello_world/models/location.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
     return Scaffold(
         appBar: AppBar(
-          title: Text('Hello'),
+          title: Text(location.name),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ImageBanner("assets/images/kiyomizu-dera.jpg"),
-            TextSection("summary", "one"),
-            TextSection("summary", "two"),
-            TextSection("summary", "three"),
-          ],
-        ));
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ImageBanner(location.imagePath),
+            ]..addAll(textSections(location))));
+  }
+
+  List<Widget> textSections(Location location) {
+    return location.facts
+        .map((fact) => TextSection(fact.title, fact.text))
+        .toList();
   }
 }
